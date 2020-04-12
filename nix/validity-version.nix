4e3cf189c76d04daf9c26267a64523ec8bb071aa
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-04-12 23:50:25 +0200

    Upgraded validity

--- nix/validity-version.nix
+++ nix/validity-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "validity";
-  rev = "be40c0a0b0bf6a1d58f4a6a5c6770c4a544c4450";
-  sha256 = "sha256:1mrn0j7kwckhzhmqg8p7p8kaw39vxp2ljjjzg550l19f3hfnj9wx";
+  rev = "62ae8c188fba688ce312579116b1b3e0b291a198";
+  sha256 = "sha256:06c2j7q1iqmgz4vnrzcsf5mki63rx5bmzp3x5rb6niz5452snq3f";
 }
---
+++ validity-version.nix
@@ -0,0 +1,6 @@
+{
+  owner = "NorfairKing";
+  repo = "validity";
+  rev = "270dd05134536151ba430174d9f31d8425e56fa4";
+  sha256 = "sha256:1ar9mgfzkk7zic8labiwhhiphplk0pwpgprd6a189mdy1izkwncw";
+}
