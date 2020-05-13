aeaf84103c817b5e4b59abfe4fe7c3228643c920
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-13 13:33:25 +0200

    upgraded the lts and validity

--- nix/validity-version.nix
+++ nix/validity-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "validity";
-  rev = "62ae8c188fba688ce312579116b1b3e0b291a198";
-  sha256 = "sha256:06c2j7q1iqmgz4vnrzcsf5mki63rx5bmzp3x5rb6niz5452snq3f";
+  rev = "95ec39a2535b9c83b1163015fd2ca28317debf42";
+  sha256 = "sha256:1my88jmgpy544frzadicpafm09kp9vjxq5wl5mb6ahjqs4ldnajx";
 }
