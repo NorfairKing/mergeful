f827db7db2a84b7b805e681b143551c5c933449f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-07-26 09:17:29 +0200

    Upgrade inputs and drop support for versions before nixos-23.05

--- nix/overrides.nix
+++ nix/overrides.nix
@@ -5,15 +5,14 @@
 }:
 with lib;
 with haskell.lib;
-self: super:
+self: _:
 let
   mergefulPkg = name: doBenchmark (buildStrictly (self.callPackage (../${name}) { }));
-  mergefulPackages =
-    {
-      mergeful = mergefulPkg "mergeful";
-      genvalidity-mergeful = mergefulPkg "genvalidity-mergeful";
-      mergeful-persistent = mergefulPkg "mergeful-persistent";
-    };
+  mergefulPackages = {
+    mergeful = mergefulPkg "mergeful";
+    genvalidity-mergeful = mergefulPkg "genvalidity-mergeful";
+    mergeful-persistent = mergefulPkg "mergeful-persistent";
+  };
 in
 {
   inherit mergefulPackages;
